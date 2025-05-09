74829f2e5c5ba6ba7b6d1bff04cf43f60c7ade0a
Boris Bunoza <borisbunoza@gmail.com>
2025-05-09 13:29:53 +0200

    First Commit

---
+++ Assignment/slave/build/zephyr/zephyr.dts.d
@@ -0,0 +1,26 @@
+empty_file.o: /home/boris/zephyr/zephyr/misc/empty_file.c \
+ /home/boris/zephyr/zephyr/boards/nxp/frdm_mcxn236/frdm_mcxn236.dts \
+ /home/boris/zephyr/zephyr/dts/arm/nxp/nxp_mcxn23x.dtsi \
+ /home/boris/zephyr/zephyr/dts/common/mem.h \
+ /home/boris/zephyr/zephyr/dts/arm/armv8-m.dtsi \
+ /home/boris/zephyr/zephyr/dts/common/skeleton.dtsi \
+ /home/boris/zephyr/zephyr/dts/arm/nxp/nxp_mcxn23x_common.dtsi \
+ /home/boris/zephyr/zephyr/include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h \
+ /home/boris/zephyr/zephyr/include/zephyr/dt-bindings/reset/nxp_syscon_reset_common.h \
+ /home/boris/zephyr/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/boris/zephyr/zephyr/include/zephyr/dt-bindings/memory-attr/memory-attr-arm.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_macro.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_internal.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_loops.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_listify.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ /home/boris/zephyr/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ /home/boris/zephyr/zephyr/include/zephyr/dt-bindings/memory-attr/memory-attr.h \
+ /home/boris/zephyr/zephyr/boards/nxp/frdm_mcxn236/frdm_mcxn236.dtsi \
+ /home/boris/zephyr/zephyr/boards/nxp/frdm_mcxn236/frdm_mcxn236-pinctrl.dtsi \
+ /home/boris/zephyr/modules/hal/nxp/dts/nxp/mcx/MCXN236VDF-pinctrl.h \
+ /home/boris/zephyr/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/boris/zephyr/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /home/boris/Desktop/Kommunikation/Examples/V17/esp-frdm/slave/frdm_mcxn236.overlay
